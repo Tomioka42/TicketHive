8cd22a28380f9ba64d75bad9187a0de90a85e89c
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-04-05 13:53:03 +0200

    Created UserRepo Not complete

---
+++ TicketHive.Server/Repos/UserRepo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketHive.Server.Data;
+
+namespace TicketHive.Server.Repos
+{
+    public class UserRepo : IUserRepo
+    {
+        private readonly AppDbContext context;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public UserRepo(AppDbContext context, SignInManager<IdentityUser> signInManager)
+        {
+            this.context = context;
+            this.signInManager = signInManager;
+        }
+    }
+}
