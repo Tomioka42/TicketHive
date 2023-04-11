edee3f6dd17c1adda32f8f223d717e49e0c5e093
Beachbay <113332198+Beachbay@users.noreply.github.com>
2023-04-11 11:56:02 +0200

    Fix login

--- TicketHive.Ui/Pages/Homepage.cshtml.cs
+++ TicketHive.Ui/Pages/Homepage.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TicketHive.Server.Data;
+using TicketHive.Server.Models;
+using TicketHive.Ui.Repos;
 
 namespace TicketHive.Ui.Pages
 {
     public class HomepageModel : PageModel
     {
-        public void OnGet()
+        private readonly IEventRepo repo;
+        private readonly AppDbContext context;
+
+        [BindProperty]
+        public List<EventModel> Events { get; set; }
+
+        public HomepageModel(IEventRepo repo, AppDbContext context)
         {
+            this.repo = repo;
+            this.context = context;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            Events = await repo.GetAllEvents();
+
+            return Page();
         }
     }
 }
