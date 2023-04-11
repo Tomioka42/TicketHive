a47f0f9630ff5b302213e46a6c7d25b4751267b0
Beachbay <113332198+Beachbay@users.noreply.github.com>
2023-04-11 11:30:26 +0200

    Registerpage

---
+++ TicketHive.Ui/Pages/Registerpage.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketHive.Ui.Pages
+{
+    [BindProperties]
+    public class RegisterpageModel : PageModel
+    {
+
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [Required(ErrorMessage = "Username is requierd")]
+        [MinLength(5, ErrorMessage = "Username must be at least 5 characters")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "Password is requierd")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string? Password { get; set; }
+
+        public RegisterpageModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            // Om alla props är bindade som de ska...
+            if (ModelState.IsValid)
+            {
+                // Skapa en ny IdentityUser med användarnamnet som är inskrivet
+                IdentityUser newUser = new()
+                {
+                    UserName = Username
+                };
+
+                // testa att registerera användaren med lösenordet den skrev in
+                var registerResult = await signInManager.UserManager.CreateAsync(newUser, Password!);
+
+                //Om registreningsförsöket lyckades...
+                if (registerResult.Succeeded)
+                {
+                    return Redirect("/Index");
+                }
+
+            }
+
+            return Page();
+        }
+    }
+}
