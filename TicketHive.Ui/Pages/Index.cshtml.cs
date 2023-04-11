462dcc37f32419eb419219dbcb9592caf2884443
Beachbay <113332198+Beachbay@users.noreply.github.com>
2023-04-11 11:23:13 +0200

    Welcomepage

--- TicketHive.Ui/Pages/Index.cshtml.cs
+++ TicketHive.Ui/Pages/Index.cshtml.cs
@@ -5,12 +5,7 @@ namespace TicketHive.Ui.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        
 
         // En kommentar från Filip
 
