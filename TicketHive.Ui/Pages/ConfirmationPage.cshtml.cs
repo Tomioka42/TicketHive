d92b8cd5b403c43e6a7950f7a0c2ef8ca675454a
AlexanderStraltsou <113332203+AlexanderStraltsou@users.noreply.github.com>
2023-04-13 12:48:32 +0200

    Confirmation Page
    - Confirmation Page

--- TicketHive.Ui/Pages/ConfirmationPage.cshtml.cs
+++ TicketHive.Ui/Pages/ConfirmationPage.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using TicketHive.Server.Models;
 
 namespace TicketHive.Ui.Pages
 {
     public class ConfirmationPageModel : PageModel
     {
+        private readonly ILogger<ConfirmationPageModel> _logger;
+
+        public CartModel? Cart { get; set; }
+
+        public ConfirmationPageModel(ILogger<ConfirmationPageModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            string cartItemsJson = HttpContext.Session.GetString("ShoppingCart"); // Session cookie
+
+            if (!string.IsNullOrEmpty(cartItemsJson))
+            {
+                // Convert the JSON string to a list of cart items
+                Cart = JsonSerializer.Deserialize<CartModel>(cartItemsJson);
+            }
+            else
+            {
+                // If the cookie doesn't exist, create a new empty cart
+                Cart = new CartModel();
+                Cart.CartItems = new List<CartItemModel>();
+            }
+
+
+            HttpContext.Session.Remove("ShoppingCart");
         }
     }
 }
